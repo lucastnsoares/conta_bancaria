9b612f4bd5e0bf0e8318cb96e1ec60c23017c57d
Lucas Soares <lucastnogsoares@gmail.com>
2025-04-13 13:02:27 -0300

    initial commit

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
-
 public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+    private String titular;
+    private String tipoConta;
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+    public void setTipoConta(String tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String nome) {
+        this.titular = nome;
+    }
+
+    public void transferencia(double valorTransferencia) {
+        if (valorTransferencia <= saldo) {
+            saldo -= valorTransferencia;
+            String msgTransferencia = String.format("""
+                    
+                    Transferência realizada com sucesso!
+                    Seu saldo atual é de R$ %.2f
+                    
+                    """, saldo);
+            System.out.println(msgTransferencia);
+        } else {
+            String msgSaldoInsuficiente = String.format("""
+                    
+                    Não há saldo suficiente para concluir a transferência.
+                    Seu saldo atual é de R$ %.2f
+                    
+                    """, saldo);
+
+            System.out.println(msgSaldoInsuficiente);
+        }
+    }
+
+    public void deposito(double valorRecebido) {
+        this.saldo += valorRecebido;
+
+        String msgDeposito = String.format("""
+                
+                Depósito realizado com sucesso!
+                Foram recebidos R$ %.2f
+                
+                Seu saldo após o deposito é de: R$ %.2f
+                """, valorRecebido, this.saldo);
+
+        System.out.println(msgDeposito);
+
+    }
 }
+
+
--- src/Principal.java
+++ src/Principal.java
@@ -1,30 +1,32 @@
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Principal {
-    public static void pressEnter() {
-        System.out.println("Pressione Enter para continuar...");
-        try {
-            while (System.in.read() != '\n') {
 
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+public class Principal {
+    public static void pressEnter() throws IOException {
+        System.out.println("Pressione Enter para continuar...");
+        System.in.read();
     }
 
-    public static void main(String[] args) {
-        double saldo = 1599.99;
-        String nome = "Lucas Soares";
-        String tipoConta = "Corrente";
+    public static void main(String[] args) throws IOException {
+
+        ContaBancaria conta = new ContaBancaria();
+        conta.setNumeroConta(55956);
+        conta.setSaldo(1599.99);
+        conta.setTitular("Tony Stark");
+        conta.setTipoConta("Corrente");
+
+
         String mensagemInicial = String.format("""
                 ******************************
                 Bem vindo a sua conta bancária!
                 
                 Nome: %s
                 Tipo de conta: %s
+                Número da conta: %s
                 Saldo: R$ %.2f
                 
-                ****************************** """, nome, tipoConta, saldo);
+                ******************************""", conta.getTitular(), conta.getTipoConta(), conta.getNumeroConta(), conta.getSaldo());
         System.out.println(mensagemInicial);
         int opcao = 0;
         while (opcao != 4) {
@@ -36,7 +38,7 @@ public class Principal {
                       3 - Receber valor
                       4 - Sair
                     
-                    Digite o número da opção desejada: """;
+                    Digite o número da opção desejada:""";
 
             System.out.println(menu);
             Scanner leitura = new Scanner(System.in);
@@ -47,7 +49,7 @@ public class Principal {
                             
                             Seu saldo é de: R$ %.2f
                             
-                            """, saldo);
+                            """, conta.getSaldo());
                     System.out.println(msgSaldo);
                     pressEnter();
                     break;
@@ -57,44 +59,16 @@ public class Principal {
                             Digite o valor que deseja transferir:
                             """);
                     double valorTransferencia = leitura.nextDouble();
-                    if (valorTransferencia <= saldo) {
-                        saldo -= valorTransferencia;
-                        String msgTransferencia = String.format("""
-                                
-                                Transferência realizada com sucesso!
-                                Seu saldo atual é de R$ %.2f
-                                
-                                """, saldo);
-                        System.out.println(msgTransferencia);
-                        pressEnter();
-                        break;
-                    } else {
-
-                        String msgSaldoInsuficiente = String.format("""
-                                
-                                Não há saldo suficiente para concluir a transferência. 
-                                Seu saldo atual é de R$ %.2f
-                                
-                                """, saldo);
-
-                        System.out.println(msgSaldoInsuficiente);
-                        pressEnter();
-                        break;
-                    }
-
+                    conta.transferencia(valorTransferencia);
+                    pressEnter();
+                    break;
                 case 3:
                     System.out.println("""
                             
                             Digite o valor do depósito:
                             """);
                     double valorRecebido = leitura.nextDouble();
-                    saldo += valorRecebido;
-
-                    System.out.println("""
-                            
-                            Foram recebidos R$ """ + valorRecebido + """
-                            
-                            Seu saldo após o deposito é de: R$ """ + saldo);
+                    conta.deposito(valorRecebido);
                     pressEnter();
                     break;
 
